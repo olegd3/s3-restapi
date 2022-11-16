4d197231e02da7d9edd59bd222e86dcc9482df55
Oleg Dudelzak <dudelzak@gmail.com>
2022-11-16 17:22:48 +0100

    Add bucket to upload request parameters

--- app/endpoints/upload.py
+++ app/endpoints/upload.py
@@ -1,3 +1,5 @@
+from typing import Optional, Union
+
 from botocore.client import BaseClient
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi import File, UploadFile
@@ -5,6 +7,7 @@ from fastapi.responses import JSONResponse
 
 from app.deps import s3_auth
 from app.s3.upload import upload_file_to_bucket
+from settings import settings
 
 router = APIRouter()
 
@@ -12,9 +15,15 @@ router = APIRouter()
 @router.post("/", status_code=status.HTTP_201_CREATED, summary="Upload files to AWS S3 Buckets",
              description="Upload a valid file to AWS S3 bucket", name="POST files to AWS S3",
              response_description="Successfully uploaded file to S3 bucket")
-def upload_file(folder: str, s3: BaseClient = Depends(s3_auth), file_obj: UploadFile = File(...)):
+def upload_file(
+        *, bucket: Union[str, None] = None,
+        folder: str,
+        s3: BaseClient = Depends(s3_auth),
+        file_obj: UploadFile = File(...)):
+    if not bucket:
+        bucket = settings.S3_DEFAULT_BUCKET
     upload_obj = upload_file_to_bucket(s3_client=s3, file_obj=file_obj.file,
-                                       bucket='testing-bucket-s3-uploader',
+                                       bucket=bucket,
                                        folder=folder,
                                        object_name=file_obj.filename
                                        )
--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.endpoints.api import api_router
+from mangum import Mangum
 
 app = FastAPI(title="S3 Buckets Info",description="Simple S3 Buckets uploader",
               openapi_url=f"/openapi.json")
 
 app.include_router(api_router)
+# handler = Mangum(app)
